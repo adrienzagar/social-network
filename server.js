24adcd0053bc4d15c6cb0dab3b9bfcc46d5f6520
adrienzagar <adrien.zagar@gmail.com>
2021-11-02 23:45:25 +0100

    feat/ jwt middleware checkuser

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 /* eslint-disable no-undef */
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/user.routes");
 require("dotenv").config({path : "./config/.env"});
 require("./config/db");
+const {checkUser} = require("./middleware/auth.middleware");
 const app = express();
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+//jwt
+app.get("*", checkUser);
 
 //routes
 app.use("/api/user", userRoutes);
