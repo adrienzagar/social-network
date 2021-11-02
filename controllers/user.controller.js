6106d25f0b72847a6c52a27a4e0f18e8179f7ae5
adrienzagar <adrien.zagar@gmail.com>
2021-11-02 01:22:12 +0100

    add/ update user bio

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -15,3 +15,28 @@ module.exports.userInfo = (req, res) => {
 		else console.log("id unknown :" + err);
 	}).select("-password");
 };
+
+module.exports.updateUser = async (req, res) => {
+	if (!ObjectID.isValid(req.params.id))
+		return res.status(400).send("ID unknown :" + req.params.id);
+
+	try {
+		await UserModel.findOneAndUpdate(
+			{_id: req.params.id},
+			{
+				$set: {
+					bio: req.body.bio
+				}
+			},
+			{new: true, upsert: true, setDefaultsOnInsert: true},
+			(err, docs) => {
+				if (!err) return res.send(docs);
+				if (err) return res.status(500).send({message: err});
+			}
+		);
+	}
+	catch (err) {
+		return res.status(500).json({message: err});
+    
+	}
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,5 +7,6 @@ router.post("/register", authController.signUp);
 // user 
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.userInfo);
+router.put("/:id", userController.updateUser);
 
 module.exports = router;
