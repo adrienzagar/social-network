3c74288bbb7f3ddc75b6b37ce0f447f762eb5f37
adrienzagar <adrien.zagar@gmail.com>
2021-11-02 01:28:34 +0100

    add/ delete user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -34,9 +34,19 @@ module.exports.updateUser = async (req, res) => {
 				if (err) return res.status(500).send({message: err});
 			}
 		);
-	}
-	catch (err) {
+	} catch (err) {
 		return res.status(500).json({message: err});
-    
+	}
+};
+
+module.exports.deleteUser = async (req, res) => {
+	if (!ObjectID.isValid(req.params.id))
+		return res.status(400).send("ID unknown :" + req.params.id);
+
+	try {
+		await UserModel.remove({_id: req.params.id}).exec();
+		res.status(200).json({message: "Successfully deleted"});
+	} catch(err) {
+		return res.status(500).json({message: err});
 	}
 };
