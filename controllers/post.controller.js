0a9651579da112f3e88d2d999a04a73b2ead9d6f
adrienzagar <adrien.zagar@gmail.com>
2021-11-10 00:22:14 +0100

    feat/ like/unlike

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,5 +1,5 @@
 const postModel = require("../models/post.model");
-const UserModel = require("../models/user.model");
+const userModel = require("../models/user.model");
 const ObjectID = require("mongoose").Types.ObjectId;
 
 module.exports.readPost = (req, res) => {
@@ -55,4 +55,66 @@ module.exports.deletePost = (req, res) => {
 			else console.log("Delete error : " + err);
 		}
 	);
+};
+
+module.exports.likePost = async (req, res) => {
+	if (!ObjectID.isValid(req.params.id))
+		return res.status(400).send("ID unknown : " + req.params.id);
+
+	try {
+		await postModel.findByIdAndUpdate(
+			req.params.id,
+			{
+				$addToSet : { likers: req.body.id}
+			},
+			{new : true},
+			(err, docs) => {
+				if (err) return res.status(400).send(err);
+			}
+		);
+		await userModel.findByIdAndUpdate(
+			req.body.id,
+			{
+				$addToSet: {likes: req.params.id}
+			},
+			{ new :true },
+			(err, docs) => {
+				if (!err) res.send(docs);
+				else return res.status(400).send; 
+			}
+		);
+	} catch (err) {
+		return res.status(400).send;
+	}
+};
+
+module.exports.unlikePost = async (req, res) => {
+	if (!ObjectID.isValid(req.params.id))
+		return res.status(400).send("ID unknown : " + req.params.id);
+  
+	try {
+		await postModel.findByIdAndUpdate(
+			req.params.id,
+			{
+				$pull: { likers: req.body.id },
+			},
+			{ new: true },
+			(err, docs) => {
+				if (err) return res.status(400).send(err);
+			}
+		);
+		await userModel.findByIdAndUpdate(
+			req.body.id,
+			{
+				$pull: { likes: req.params.id },
+			},
+			{ new: true },
+			(err, docs) => {
+				if (!err) res.send(docs);
+				else return res.status(400).send(err);
+			}
+		);
+	} catch (err) {
+		return res.status(400).send(err);
+	}
 };
